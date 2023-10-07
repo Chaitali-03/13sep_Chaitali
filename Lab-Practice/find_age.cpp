9c682ca67945cdb24e642e53eaf251832d436b6a
Chaitali-03 <145127374+Chaitali-03@users.noreply.github.com>
2023-10-07 17:03:35 +0530

    Update find_age.c

--- Lab-Practice/find_age.cpp
+++ Lab-Practice/find_age.cpp
@@ -1,18 +1,19 @@
+//WAP to find previous and next year age from given age
 #include<stdio.h>
- 
+//
 int main()
 {
-	int age,prev,next;
+	int age,prev,next;	//creat the variable
 	
-	printf("enter your current age:");
-	scanf("%d",&age);
+	printf("enter your current age:");	//to get the value
+	scanf("%d",&age);	//to store the value
 	
-	prev = --age;
+	prev = --age;	//unary operator (decrement) 
+	next = ++age;	//unary operator (increment) 
 	next = ++age;
-	next = ++age;
 	
-	printf("your prev year age is %d \n",prev);
-	printf("your next year age is %d \n",next);
+	printf("your prev year age is %d \n",prev);	//print previous year age
+	printf("your next year age is %d \n",next);	//print next year age
 	
 	return 0;
 	
