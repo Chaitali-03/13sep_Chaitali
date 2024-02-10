1b33c671daf1ea16f86eaf148cbd3cff85bfd129
Chaitali-03 <145127374+Chaitali-03@users.noreply.github.com>
2024-02-10 19:08:07 +0530

    Add files via upload

---
+++ Flutter/Module-02/Q.47.dart
@@ -0,0 +1,38 @@
+//47. Create a program using Set.
+void main()
+{
+
+  Set<dynamic> set = Set();
+  //set will remove duplicate elements
+  set.add("a");
+  set.add("b");
+  set.add("c");
+  set.add("d");
+  set.add("a");
+  set.add(5);
+
+  print(set);
+
+  Set<int>set2 = Set();
+  set2.add(1);
+  set2.add(2);
+  set2.add(3);
+  set2.add(4);
+
+  set.addAll(set2);
+
+
+
+  Set<String>set3 = Set();
+  set3.add("Android");
+  set3.add("Testing");
+  set3.add("Flutter");
+  set3.add("Flutter");
+  set3.add("Java");
+
+  set.addAll(set3);
+
+  print(set2);
+  print(set3);
+  print(set);
+}
