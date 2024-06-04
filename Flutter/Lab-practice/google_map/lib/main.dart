8d9f454d61c177245d31fa33cf588a9c6072191d
Chaitali-03 <145127374+Chaitali-03@users.noreply.github.com>
2024-06-04 16:00:30 +0530

    commit

---
+++ Flutter/Lab-practice/google_map/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp(),debugShowCheckedModeBanner: false));
+}
+class MyApp extends StatelessWidget {
+
+  late GoogleMapController mapController;
+  LatLng _latLng = LatLng(22.290374, 70.775374);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Google Map"),backgroundColor: Colors.blueAccent,),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+            target: _latLng,
+            zoom: 11,
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ Flutter/Lab-practice/otp_varify/android/app/src/main/kotlin/com/example/otp_varify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.otp_varify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
